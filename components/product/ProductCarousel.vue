1de6dfa666a8a5a0efb27a47854c5524937317c6
adam bezemek <adam@tap.inc>
2020-11-10 22:50:49 -0600

    prod build adjustments

--- components/product/ProductCarousel.vue
+++ components/product/ProductCarousel.vue
@@ -5,13 +5,25 @@
         <img src="~/assets/woman-running.jpg" alt="Woman Running" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/436x384" alt="Placeholder" />
+        <img
+          src="https://via.placeholder.com/436x384"
+          loading="lazy"
+          alt="Placeholder"
+        />
       </div>
       <div>
-        <img src="https://via.placeholder.com/436x384" alt="Placeholder" />
+        <img
+          src="https://via.placeholder.com/436x384"
+          loading="lazy"
+          alt="Placeholder"
+        />
       </div>
       <div>
-        <img src="https://via.placeholder.com/436x384" alt="Placeholder" />
+        <img
+          src="https://via.placeholder.com/436x384"
+          loading="lazy"
+          alt="Placeholder"
+        />
       </div>
     </VueSlickCarousel>
   </div>
@@ -19,38 +31,9 @@
 
 <script>
 import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
-// optional style for arrows & dots
-import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
   name: "ProductCarousel",
   components: { VueSlickCarousel }
 };
 </script>
-
-<style>
-.slick-prev {
-  left: 0;
-  z-index: 1;
-}
-.slick-next {
-  right: 0;
-  z-index: 1;
-}
-.slick-dots {
-  bottom: 5px;
-}
-.slick-dots li {
-  margin: 0;
-}
-.slick-dots li.slick-active button:before {
-  opacity: 1;
-  color: white;
-}
-@media (min-width: 1024px) {
-  .carousel-max-width {
-    max-width: 436px;
-  }
-}
-</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,9 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    './vue-slick-carousel/dist/vue-slick-carousel.css',
+    './vue-slick-carousel/dist/vue-slick-carousel-theme.css',
+    '~/css/carousel-overrides.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -33,8 +36,7 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    'nuxt-purgecss',
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
